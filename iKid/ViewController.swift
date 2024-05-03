ea34d007818170403dc00212f9bcd2a7bd4316df
Erickuokuo <eric_kuo@outlook.com>
2024-05-03 15:14:37 -0700

    innit

---
+++ iKid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Eric Kuo on 5/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
